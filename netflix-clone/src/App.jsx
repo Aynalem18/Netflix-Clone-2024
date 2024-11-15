afa89a2be2209bb9bf17bdcae13b4e45edf94de6
Aynalem18 <aynalemfisum@gmail.com>
2024-11-14 23:03:20 -0500

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
-import './App.css'
+import Home from './components/Pages/Home/Home'
+// import './App.css'
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello</h1>
+      <Home/>
     </>
   )
 }
